35482bc1bc11484e2ff3888393dcf856c2e37190
Grigalgol <grisha101202@yandex.ru>
2022-05-01 20:38:13 +0300

    fix RegisterController

--- src/main/java/com/example/barbershop/web/UserRegistrationController.java
+++ src/main/java/com/example/barbershop/web/UserRegistrationController.java
@@ -4,7 +4,9 @@ import com.example.barbershop.service.UserService;
 import com.example.barbershop.web.dto.UserRegistrarionDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,8 +16,14 @@ public class UserRegistrationController {
     @Autowired
     private UserService userService;
 
-    private String registerUserAccount(@ModelAttribute("user") UserRegistrarionDto userRegistrarionDto) {
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrarionDto userRegistrarionDto) {
         userService.save(userRegistrarionDto);
         return "redirect:/registration?success";
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registrarion";
     }
 }
